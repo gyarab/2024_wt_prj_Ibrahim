f4b365b9e326303ab7ab04007b9e557f07501477
Adrian Ibrahim <adrian.ibrahim.s@gyarab.cz>
2025-03-10 20:37:25 +0100

    commit

--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('one',TemplateView.as_view(template_name='main/one.html')),
-        path('two',TemplateView.as_view(template_name='main/two.html')),
+    path('one.html',TemplateView.as_view(template_name='main/one.html')),
+        path('two.html',TemplateView.as_view(template_name='main/two.html')),
 
 ]
