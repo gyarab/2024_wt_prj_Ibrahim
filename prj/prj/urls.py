53da7b9b2535be8d543f4cfa15d01fc148049408
AdrianIbrahim <adrian.ibrahim.s@gyarab.cz>
2025-03-14 12:15:20 +0100

    skola

--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='main/home.html'), name='home'),  # Hlavní stránka
-    path('detail/', TemplateView.as_view(template_name='main/ball_detail.html'), name='ball_detail'),  # Detail míče
+    path('home', TemplateView.as_view(template_name='main/home.html'), name='home'),  # Hlavní stránka
+    path('detail/', TemplateView.as_view(template_name='main/ball_detail.html'), name='detail'),  # Detail míče
 ]
