6bcf0ea07aaff789b7185820de6926dd15583b05
AdrianIbrahim <adrian.ibrahim.s@gyarab.cz>
2025-03-21 12:20:00 +0100

    done

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Ball
+admin.site.register(Ball)
 
 # Register your models here.
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+class Ball(models.Model):
+    name = models.CharField(max_length=300)
+    price = models.IntegerField(null=True, blank=True)
+    hmotnost =  models.IntegerField(null=True, blank=True)
+    obvod =  models.IntegerField(null=True, blank=True)
+    ucel = models.CharField(max_length = 70)
+    znacka = models.ForeignKey('Znacka', on_delete=models.SET_NULL)
+    def __str__(self):
+     return f"{self.name} "
 # Create your models here.
+
+class Znacka(models.Model):
+    znacka = models.CharField(max_length=300)
+    
+        
+    def __str__(self):
+     return f"{self.znacka} "
