5f3e0eca03747002c6f3b3c1427a159405b95a65
AdrianIbrahim <adrian.ibrahim.s@gyarab.cz>
2025-03-28 11:31:23 +0100

    fixed

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Ball
 admin.site.register(Ball)
 
+class BallAdmin(admin.ModelAdmin):
+    list_display = {"id","name","price","hmotnost"}
+
+    admin.site.register(id,name)
 # Register your models here.
--- prj/main/models.py
+++ prj/main/models.py
@@ -3,16 +3,16 @@ from django.db import models
 class Ball(models.Model):
     name = models.CharField(max_length=300)
     price = models.IntegerField(null=True, blank=True)
-    hmotnost =  models.IntegerField(null=True, blank=True)
-    obvod =  models.IntegerField(null=True, blank=True)
-    ucel = models.CharField(max_length = 70)
-    znacka = models.ForeignKey('Znacka', on_delete=models.SET_NULL)
+    weight =  models.IntegerField(null=True, blank=True)
+    diameter =  models.IntegerField(null=True, blank=True)
+    purpose = models.CharField(max_length = 70)
+    brand = models.CharField(max_length = 70)
     def __str__(self):
      return f"{self.name} "
 # Create your models here.
 
-class Znacka(models.Model):
-    znacka = models.CharField(max_length=300)
+class Brand(models.Model):
+    brand = models.CharField(max_length=300)
     
         
     def __str__(self):
