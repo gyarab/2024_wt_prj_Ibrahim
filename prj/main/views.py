af02d6ff8c26dfca4c9f9444e936c160d942e5c2
Adrian Ibrahim <adrian.ibrahim.s@gyarab.cz>
2025-04-04 15:37:50 +0200

    ukol oprava

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+#from .models import Mic
 
+#def mic_list(request):
+  # for mic in mice:
+       # mic.image_url = f'{mic.name.lower()}.jpg' # URL obrázku
+    #return render(request, 'mic_list.html', {'mice': mice})
 # Create your views here.
