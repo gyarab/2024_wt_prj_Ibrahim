ef295f8e554bb9b31d5d2628a244dbe4c49ccb02
AdrianIbrahim <adrian.ibrahim.s@gyarab.cz>
2025-05-02 12:17:16 +0200

    Přidání vyhledávání míčů podle názvu a značky

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
-from .models import Ball
 from django.shortcuts import render, get_object_or_404
 from .models import Ball
+from django.db.models import Q  # Pro pokročilé filtrování
 
-
-
+# Zobrazení seznamu míčů s vyhledáváním
 def ball_list(request):
-    balls = Ball.objects.all().order_by('name')  # Order balls by name alphabetically
+    query = request.GET.get('q')  # Získáme dotaz z parametru URL
+    if query:
+        balls = Ball.objects.filter(
+            Q(name__icontains=query) | Q(brand__icontains=query)
+        ).order_by('name')
+    else:
+        balls = Ball.objects.all().order_by('name')
+    
     return render(request, 'main/ball_list.html', {'balls': balls})
 
-
-
-
+# Detail míče podle primárního klíče
 def ball_detail(request, pk):
     ball = get_object_or_404(Ball, pk=pk)
     return render(request, 'main/ball_detail.html', {'ball': ball})
-
